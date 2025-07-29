dcb4fd5ffb62d95f84e9f6d6ad3b423f5c0b857a
Ondrej Rohon <ondrej.rohon@gmail.com>
2025-07-29 14:41:43 +0200

    prepare for publish

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -6,8 +6,6 @@ if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 const dbUrl = process.env.NODE_ENV === 'test' ? env.DATABASE_TEST_URL : env.DATABASE_URL;
 
-console.log('env', process.env.NODE_ENV, 'dbUrl', dbUrl);
-
 const client = postgres(dbUrl);
 
 export const db = drizzle(client);
